91b141c44c355035270b9836e68b159322439a05
Roman Vinnick <romanvinnick@gmail.com>
2023-12-24 11:15:33 +0100

    instruction added

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -9,6 +9,8 @@ export const Credits = () => {
           target="_blank"
         >
          Made by @Roman1510
+         <br/>
+         <span style={{fontSize:'9px'}}>Just scroll down and don't forget to turn on the star ⭐</span>
         </a>
       </div>
       
